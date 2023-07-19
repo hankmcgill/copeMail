7a455a21bdefa6f40aae5447f8db6d80ba030654
Hank McGill <henrymcgill@gmail.com>
2023-07-18 20:23:51 -0400

    finished auth portion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const storedUsers = {}
+
 module.exports = {
   // simplified version of checking if email is valid by checking chars
   checkIfValidEmail: (req, res, next) => {
@@ -6,5 +8,15 @@ module.exports = {
       return next({ status: 500, msg: 'Invalid input! Did you enter your email correctly?' })
     }
     return next()
+  },
+
+  // simulated version of checking if email is stored in database - obviously normally happens as query to external DB
+  checkIfAcctExists: (req, res, next) => {
+    if (storedUsers[req.body.email]) return next({ status: 500, msg: 'Sorry, that email is associated with an account already :)' })
+    else {
+      // if not taken, can be used for a new acct (value set to current date to simulate values stored in a DB)
+      storedUsers[req.body.email] = Date.now()
+      return next()
+    }
   }
 }
