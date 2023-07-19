bd4d59a72a0761569315b5161a89f9064aee0064
Hank McGill <henrymcgill@gmail.com>
2023-07-18 21:40:28 -0400

    set up most interval logic

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,7 +5,23 @@ module.exports = {
     // create closure of unique emails & validity for each unique user
     const userCache = { ...emails }
 
+    const getValidEmails = () => {
+      // grab all valid (set to true) msgs
+      const msgArr = Object.keys(userCache).filter(valid => userCache[valid])
+
+      // base case: none left
+      if (msgArr.length === 0) clearInterval(interval)
+
+      // else randomly select, send, and invalidate message
+      const selected = msgArr[Math.floor(Math.random() * msgArr.length)]
+      // "send" message
+      console.log('SENT:', selected)
+    }
+
     // iterate thru emails, sending one per second at random until there are none left
+    const interval = setInterval(() => {
+      getValidEmails()
+    }, 1000)
 
     return next()
   }
