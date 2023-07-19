8c9eeefc2b5831fa9205c649be714e05fb2fc050
Hank McGill <henrymcgill@gmail.com>
2023-07-19 13:35:29 -0400

    finished MVP

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -10,18 +10,30 @@ module.exports = {
       const msgArr = Object.keys(userCache).filter(valid => userCache[valid])
 
       // base case: none left
-      if (msgArr.length === 0) clearInterval(interval)
+      if (msgArr.length === 0) {
+        console.log('erwrwe' + interval)
+
+        clearInterval(interval) // end process for unique user
+        console.log(`ALL MESSAGES SENT to ${req.body.email} -  Thank you!`)
+        return
+      }
 
       // else randomly select, send, and invalidate message
       const selected = msgArr[Math.floor(Math.random() * msgArr.length)]
       // "send" message
-      console.log('SENT:', selected)
+      console.log(`SENT to: ${req.body.email} ${selected}`)
+
+      for (const key in userCache) {
+        if (key === selected) {
+          userCache[key] = false // disable sent message as no longer valid for unique user
+        }
+      }
     }
 
     // iterate thru emails, sending one per second at random until there are none left
     const interval = setInterval(() => {
       getValidEmails()
-    }, 1000)
+    }, 60000)
 
     return next()
   }
